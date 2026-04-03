0609346b740b3945b611a1c8f73010152004876c
Craig Pellegrino <craigpellegrino@gmail.com>
2026-04-03 11:47:19 -0400

    feat: add plotting of instrument footprints and visibility windows

--- across/client/apis/instrument.py
+++ across/client/apis/instrument.py
@@ -1,7 +1,77 @@
 from datetime import datetime
 
+import plotly.graph_objects as go
+
 import across.sdk.v1 as sdk
 from across.sdk.v1.api_client_wrapper import ApiClientWrapper
+from across.tools.core.plotting import plot_footprint
+from across.tools.core.schemas import Coordinate, Polygon
+
+
+class CustomInstrument(sdk.Instrument):
+    """
+    Class to extend the sdk.Instrument object to add
+    plotting functionality.
+    """
+
+    def plot_footprint(
+        self,
+        fig: go.Figure | None = None,
+        name: str | None = None,
+        color: str | None = None,
+        lat_axis_tick: int = 30,
+        lon_axis_tick: int = 60,
+    ) -> go.Figure:
+        """
+        Method to plot the footprint using plotly.
+        Calls the across-tools plotting core functionality and configures the plot
+        layout to user specifications.
+
+        Parameters
+        ----------
+        fig : go.Figure, optional
+            An existing plotly figure to add the footprint to, by default None
+        name : str | None, optional
+            The name to assign to the detector traces, by default None
+        color : str | None, optional
+            The color to assign to the detector traces, by default None
+        lat_axis_tick : int, optional
+            The latitude axis tick interval, by default 30
+        lon_axis_tick : int, optional
+            The longitude axis tick interval, by default 60
+        Returns
+        -------
+        go.Figure
+            The plotly figure containing the footprint plot
+        """
+        if fig is None:
+            fig = go.Figure()
+            fig.update_layout(
+                title="Footprint Visualization",
+                geo=dict(
+                    projection_type="mollweide",
+                    showland=False,
+                    showcountries=False,
+                    showcoastlines=False,
+                    lataxis=dict(showgrid=True, dtick=lat_axis_tick),
+                    lonaxis=dict(showgrid=True, dtick=lon_axis_tick),
+                ),
+            )
+
+        if self.footprints is not None:
+            detectors = []
+            for footprint in self.footprints:
+                coordinates = [Coordinate(ra=coord.x, dec=coord.y) for coord in footprint]
+                detectors.append(Polygon(coordinates=coordinates))
+
+            fig = plot_footprint(
+                detectors=[detector.model_dump() for detector in detectors],
+                name=name,
+                fig=fig,
+                color=color,
+            )
+
+        return fig
 
 
 class Instrument:
@@ -22,7 +92,7 @@ class Instrument:
         """
         self.across_client = across_client
 
-    def get(self, id: str) -> sdk.Instrument:
+    def get(self, id: str) -> CustomInstrument:
         """
         Retrieve a single Instrument by ID.
 
@@ -34,7 +104,18 @@ class Instrument:
             sdk.Instrument:
                 The requested Instrument object.
         """
-        return sdk.InstrumentApi(self.across_client).get_instrument(instrument_id=id)
+        sdk_instrument = sdk.InstrumentApi(self.across_client).get_instrument(instrument_id=id)
+        return CustomInstrument(
+            id=sdk_instrument.id,
+            created_on=sdk_instrument.created_on,
+            name=sdk_instrument.name,
+            short_name=sdk_instrument.short_name,
+            telescope=sdk_instrument.telescope,
+            footprints=sdk_instrument.footprints,
+            filters=sdk_instrument.filters,
+            constraints=sdk_instrument.constraints,
+            visibility_type=sdk_instrument.visibility_type,
+        )
 
     def get_many(
         self,
@@ -42,7 +123,7 @@ class Instrument:
         telescope_name: str | None = None,
         telescope_id: str | None = None,
         created_on: datetime | None = None,
-    ) -> list[sdk.Instrument]:
+    ) -> list[CustomInstrument]:
         """
         Retrieve multiple instruments filtered by optional criteria.
 
@@ -60,9 +141,23 @@ class Instrument:
             list[sdk.Instrument]:
                 A list of instruments matching the given filters.
         """
-        return sdk.InstrumentApi(self.across_client).get_instruments(
+        sdk_instruments = sdk.InstrumentApi(self.across_client).get_instruments(
             name=name,
             telescope_name=telescope_name,
             telescope_id=telescope_id,
             created_on=created_on,
         )
+        return [
+            CustomInstrument(
+                id=sdk_instrument.id,
+                created_on=sdk_instrument.created_on,
+                name=sdk_instrument.name,
+                short_name=sdk_instrument.short_name,
+                telescope=sdk_instrument.telescope,
+                footprints=sdk_instrument.footprints,
+                filters=sdk_instrument.filters,
+                constraints=sdk_instrument.constraints,
+                visibility_type=sdk_instrument.visibility_type,
+            )
+            for sdk_instrument in sdk_instruments
+        ]
